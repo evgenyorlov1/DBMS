dfc4ec77753aa76d60280b8df0f38b7fb371fae6
orlov <evgenyorlov1@gmail.com>
2016-10-05 15:20:01 +0300

    REFACTOR TextPanel.java

--- src/dbms/TextPanel.java
+++ src/dbms/TextPanel.java
@@ -65,195 +65,50 @@ public class TextPanel extends javax.swing.JPanel {
         if((evt.getKeyCode() == KeyEvent.VK_ENTER) && (evt.getID() == KeyEvent.KEY_PRESSED)) {
             String currentText = jTextPane1.getText();
             String lastLine = currentText.split("\n")
-                    [currentText.split("\n").length-1];
-            ArrayList<String[]> results = new ArrayList<>();
-                    
-            System.out.println("before use: " + useState);
-            useState = QuaeryProcessor.useState(lastLine, useState);
-            System.out.println("after use: " + useState);
-            Object[] command = QuaeryProcessor.parse(lastLine);                                            
+                    [currentText.split("\n").length-1];           
+                      
+            useState = QuaeryProcessor.useState(lastLine, useState);            
+            Object[] command = QuaeryProcessor.parse(lastLine);  
             
-            //FIX use state issue            
-            //REFACTOR create function for jTextPane.setText()
-            switch((int)command[0]) {
-                //clear  TESTED
+            switch((int)command[0]) {                
                 case 0:   
-                    System.out.println("\nclear");
-                    jTextPane1.setText(""); //FIX caret on second line                    
-                    break;
-                    
-                //show dbs TESTED
+                    clear();             
+                    break;                                    
                 case 1:
-                    System.out.println("\nshow dbs");
-                    ArrayList<String> dbs = dbms.show_dbs();                    
-                    for(int i=0; i<dbs.size(); i++) {
-                        System.out.println(dbs.get(i));
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(dbs.get(i)), 
-                                    null); 
-                        } catch(Exception e) {}
-                    }
-                    break;
-                    
-                //use db_name    TESTED             
+                    try {show_dbs();} catch(Exception e) {}
+                    break;                                    
                 case 2:
-                    System.out.println("\nuse");
-                    System.out.println(useState);
-                    if(dbms.is_unique_name(useState)) {
-                        System.out.println("TextPanel.create_database"); //have command that creates table
-                        dbms.create_database(useState);
-                    } else {
-                        System.out.println("TextPanel.select_database");
-                        //dbms.create_database(useState);                    
-                    }
-                    break;                   
-                //db - show current database TESTED
+                    use();
+                    break;                                   
                 case 10:
-                    System.out.println("\ndb");
-                    if(!useState.equals("none")) 
-                        jTextPane1.setText(useState + "\n");
-                    else 
-                        jTextPane1.setText("select database with use");
-                    break;
-                //show tables TESTED
+                    db();
+                    break;                
                 case 11:
-                    System.out.println("\nshow tables");
-                    if(!useState.equals("none")) {
-                        ArrayList<String> tables = dbms.show_tables(useState);
-                        for(int i=0; i<tables.size(); i++) {
-                            try {
-                                jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(tables.get(i)), 
-                                    null); 
-                            } catch(Exception e) {}                            
-                        }  
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }
-                    break;
-                //db.dropDatabase() TESTED               
+                    try {show_tables(lastLine);} catch(Exception e) {}                    
+                    break;                
                 case 12:
-                    System.out.println("\ndropDatabase");
-                    if(!useState.equals(this.none)) {
-                        dbms.drop_database(useState);
-                        useState = this.none;
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                    
-                    break;
-                //db.createTable(###)  TESTED            
+                    try {dropDatabase(lastLine);} catch(Exception e) {}                    
+                    break;                
                 case 13:
-                    System.out.println("\ncreateTable");
-                    if(!useState.equals("none")) {
-                        dbms.create_table(useState, (String)command[1]);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                            
-                    break;
-                //db.save()    
+                    try {createTable(command, lastLine);} catch(Exception e) {}                                            
+                    break;    
                 case 14:
-                    System.out.println("\nsave");
-                    if(!useState.equals("none")) {
-                        dbms.save(useState);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }        
-                    break;                    
-                //db.###.drop()      TESTED            
+                    try {save(lastLine);} catch(Exception e) {}
+                    break;                                    
                 case 101:
-                    System.out.println("\ndrop");
-                    if(!useState.equals("none")) {
-                        dbms.drop_table(useState, (String)command[1]);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                            
-                    break;
-                //db.###.find()    
-                case 102:
-                    System.out.println("\nfind");
-                    if(!useState.equals("none")) {
-                        results.clear();
-                        results = dbms.find(useState, (String)command[1]);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                           
-                    break;                    
-                //db.###.find().limit(###)                   
+                    try {drop(command, lastLine);} catch(Exception e) {}              
+                    break;                
+                case 102:                    
+                    try {find(command, lastLine);} catch(Exception e) {}                                                      
+                    break;                                    
                 case 1001:
-                    System.out.println("\nlimit");
-                    if(!useState.equals("none")) {
-                        results.clear();                    
-                        results = dbms.limit(useState, (String)command[1], (int)command[2]);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                    
-                    break;
-                //db.###.find().sort({key:[-1;1]})                 
+                    try {limit(command, lastLine);} catch(Exception e) {}              
+                    break;                
                 case 1002:
-                    System.out.println("\nTextPanel.sort");
-                    if(!useState.equals("none")) {
-                        results.clear();                    
-                        results = dbms.sort(useState, (String)command[1], (String)command[2], (int)command[3]);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                                 
-                    break;
-                //db.###.find().skip(###)                
+                    try {sort(command, lastLine);} catch(Exception e) {}              
+                    break;                
                 case 1003:       
-                    if(!useState.equals("none")) {
-                        results.clear();
-                        results = dbms.skip(useState, (String)command[1], (int)command[2]);
-                    } else {
-                        try {
-                            jTextPane1.getDocument().insertString(
-                                    jTextPane1.getText().length(), 
-                                    "\n".concat(lastLine.concat(" - select database")), 
-                                    null);                        
-                        } catch(Exception e) {}
-                    }                          
+                    try {skip(command, lastLine);} catch(Exception e) {}              
                     break;                    
                 case 999:                
                     try {
@@ -266,6 +121,135 @@ public class TextPanel extends javax.swing.JPanel {
         }            
     }//GEN-LAST:event_EnterPressed
 
+    private void clear() {
+        System.out.println("\nclear");
+        jTextPane1.setText(""); //FIX caret on second line    
+    }
+    
+    private void show_dbs() throws Exception{
+        System.out.println("\nshow dbs");
+        ArrayList<String> dbs = dbms.show_dbs();  
+        
+        for(String db : dbs) {
+            System.out.println(db);            
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(db), null);             
+        }
+    }
+    
+    private void use() {
+        System.out.println("\nuse");
+        System.out.println(useState);
+        if(dbms.is_unique_name(useState))          
+            dbms.create_database(useState);
+        else 
+            System.out.println("TextPanel.select_database");            
+    }
+    
+    private void db() {
+        System.out.println("\ndb");
+        if(!useState.equals("none")) 
+            jTextPane1.setText(useState + "\n");
+        else 
+            jTextPane1.setText("select database with use");
+    }
+    
+    private void show_tables(String lastLine) throws Exception{
+        System.out.println("\nshow tables");
+        if(!useState.equals(this.none)) {
+            ArrayList<String> tables = dbms.show_tables(useState);
+            for(String tb : tables)             
+                jTextPane1.getDocument().insertString(jTextPane1.getText().
+                        length(), "\n".concat(tb), null);                                                      
+        } else {           
+            jTextPane1.getDocument().insertString( jTextPane1.getText().length(), 
+                    "\n".concat(lastLine.concat(" - select database")), null);                                    
+        }
+    }
+    
+    private void dropDatabase(String lastLine) throws Exception{
+        System.out.println("\ndropDatabase");
+        if(!useState.equals(this.none)) {
+            dbms.drop_database(useState);
+            useState = this.none;
+        } else {            
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);
+        }    
+    }
+    
+    private void createTable(Object[] command, String lastLine) throws Exception {
+        System.out.println("\ncreateTable");
+        if(!useState.equals("none")) {
+            dbms.create_table(useState, (String)command[1]);
+        } else {            
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);                                    
+        }   
+    }
+    
+    private void save(String lastLine) throws Exception{
+        System.out.println("\nsave");
+        if(!useState.equals(this.none)) {
+            dbms.save(useState);
+        } else {
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);                                    
+        }    
+    }
+    
+    private void drop(Object[] command, String lastLine) throws Exception{
+        System.out.println("\ndrop");
+        if(!useState.equals(this.none)) {
+            dbms.drop_table(useState, (String)command[1]);
+        } else {            
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);                                    
+        }      
+    }
+    
+    private void find(Object[] command, String lastLine) throws Exception {
+        System.out.println("\nfind");
+        ArrayList<String[]> results = new ArrayList<>();
+        if(!useState.equals(this.none)) {            
+            results = dbms.find(useState, (String)command[1]);
+        } else {           
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);                                   
+        }       
+    }
+    
+    private void limit(Object[] command, String lastLine) throws Exception {
+        System.out.println("\nlimit");
+        ArrayList<String[]> results = new ArrayList<>();
+        if(!useState.equals(this.none)) {                  
+            results = dbms.limit(useState, (String)command[1], (int)command[2]);
+        } else {           
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);                                   
+        }     
+    }
+    
+    private void sort(Object[] command, String lastLine) throws Exception{
+        System.out.println("\nTextPanel.sort");
+        ArrayList<String[]> results = new ArrayList<>();
+        if(!useState.equals(this.none)) {                                
+            results = dbms.sort(useState, (String)command[1], (String)command[2], (int)command[3]);
+        } else {            
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);
+        }     
+    }
+    
+    private void skip(Object[] command, String lastLine) throws Exception{
+        ArrayList<String[]> results = new ArrayList<>();
+        if(!useState.equals(this.none)) {            
+            results = dbms.skip(useState, (String)command[1], (int)command[2]);
+        } else {            
+            jTextPane1.getDocument().insertString(jTextPane1.getText().length(), 
+                "\n".concat(lastLine.concat(" - select database")), null);                                    
+        }  
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JScrollPane jScrollPane1;
